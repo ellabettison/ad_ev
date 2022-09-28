8bd9ed7242b30e54279df6ea5d315c182a62ca68
ellabettison <ella.bettison@gmail.com>
2022-09-28 08:45:10 +0100

    Prey randomly samples from actual or generated distributions

--- predator.py
+++ predator.py
@@ -2,33 +2,46 @@ import torch.nn as nn
 import torch
 import numpy as np
 
+layer_sizes = [2,64,32,1]
+
+consonants = "qwrtpsdfghjklzxcvbnm"
+vowels = "eyuioa"
+
 class Predator:
     def __init__(self, genes=None):
         if genes is None:
-            self.genes = np.random.normal(loc=0, scale=0.1, size=2 * 128 + 128 * 64 + 64 * 1)
+            self.genes = np.random.normal(loc=0, scale=0.1, size=layer_sizes[0] * layer_sizes[1] + 
+                                                                 layer_sizes[1] * layer_sizes[2] + 
+                                                                layer_sizes[2 ] * layer_sizes[3])
         else:
             self.genes = genes
         self.model = self.generate_network()
+        self.food = 0
+        self.name = np.random.choice([*consonants]) +np.random.choice([*vowels]) +np.random.choice([*consonants])
 
     def generate_network(self):
         model = nn.Sequential(
-            nn.Linear(2, 128),
+            nn.Linear(layer_sizes[0], layer_sizes[1]),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(128, 64),
+            nn.Linear(layer_sizes[1], layer_sizes[2]),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(64, 1),
+            nn.Linear(layer_sizes[2], layer_sizes[3]),
             nn.Sigmoid(),
         )
         
         with torch.no_grad():
-            self.model[0].weights = nn.Parameter(self.genes[:len(self.model[0].weights)])
-            self.model[1].weights = nn.Parameter(self.genes[len(self.model[0].weights):len(self.model[0].weights)+len(self.model[1].weights)])
-            self.model[2].weights = nn.Parameter(self.genes[-len(self.model[2].weights):])
+            torch.tensor(self.genes[:])
+            
+            model[0].weight.data = nn.Parameter(torch.reshape(torch.tensor(self.genes[:layer_sizes[0]*layer_sizes[1]]), (layer_sizes[1], layer_sizes[0])))
+            model[3].weight.data = nn.Parameter(torch.reshape(torch.tensor(self.genes[layer_sizes[0]*layer_sizes[1]:layer_sizes[0]*layer_sizes[1]+layer_sizes[1]*layer_sizes[2]]),(layer_sizes[2], layer_sizes[1])))
+            model[6].weight.data = nn.Parameter(torch.reshape(torch.tensor(self.genes[-layer_sizes[2]*layer_sizes[3]:]),(layer_sizes[3], layer_sizes[2])))
         
         return model
 
-    def generate(self):
-        noise_inp = np.random.randn(2)
-        return self.model(noise_inp)
+    def discriminate(self, inp):
+        return self.model(inp)
+    
+    def add_food(self, amt):
+        self.food += amt
--- prey.py
+++ prey.py
@@ -2,14 +2,22 @@ import torch.nn as nn
 import torch
 import numpy as np
 
+layer_sizes = [2, 16, 32, 2]
+
+consonants = "qwrtpsdfghjklzxcvbnm"
+vowels = "eyuioa"
 
 class Prey:
     def __init__(self, genes=None):
         if genes is None:
-            self.genes = np.random.normal(loc=0, scale=0.1, size=2 * 16 + 16 * 32 + 32 * 2)
+            self.genes = np.random.normal(loc=0, scale=0.1, size=layer_sizes[0] * layer_sizes[1] +
+                                                                 layer_sizes[1] * layer_sizes[2] +
+                                                                 layer_sizes[2] * layer_sizes[3])
         else:
             self.genes = genes
         self.model = self.generate_network()
+        self.to_remove = False
+        self.name = np.random.choice([*consonants]) +np.random.choice([*vowels]) +np.random.choice([*consonants])
 
     def generate_network(self):
         model = nn.Sequential(
@@ -18,15 +26,39 @@ class Prey:
             nn.Linear(16, 32),
             nn.ReLU(),
             nn.Linear(32, 2),
+            # nn.Sigmoid()
         )
         with torch.no_grad():
-            self.model[0].weights = nn.Parameter(self.genes[:len(self.model[0].weights)])
-            self.model[1].weights = nn.Parameter(
-                self.genes[len(self.model[0].weights):len(self.model[0].weights) + len(self.model[1].weights)])
-            self.model[2].weights = nn.Parameter(self.genes[-len(self.model[2].weights):])
+            model[0].weight.data = nn.Parameter(
+                torch.reshape(torch.tensor(self.genes[:layer_sizes[0] * layer_sizes[1]]), (layer_sizes
+                                                                                           [1], layer_sizes[0])))
+            model[2].weight.data = nn.Parameter(torch.reshape(torch.tensor(self.genes[
+                                                                           layer_sizes[0] * layer_sizes[1]:layer_sizes[
+                                                                                                               0] *
+                                                                                                           layer_sizes[
+                                                                                                               1] +
+                                                                                                           layer_sizes[
+                                                                                                               1] *
+                                                                                                           layer_sizes[
+                                                                                                               2]]),
+                                                              (layer_sizes[2], layer_sizes[1])))
+            model[4].weight.data = nn.Parameter(
+                torch.reshape(torch.tensor(self.genes[-layer_sizes[2] * layer_sizes[3]:]),
+                              (layer_sizes[3], layer_sizes[2])))
 
         return model
 
     def generate(self):
-        noise_inp = np.random.randn(2)
-        return self.model(noise_inp)
+        noise_inp = torch.tensor(np.random.randn(2))
+        return self.model(noise_inp), 1
+
+    def generate_or_sample(self):
+        real_or_fake = np.random.random()
+        if real_or_fake < 0.5:
+            return self.generate()
+        return self.sample_distribution()
+
+    def sample_distribution(self):
+        rand_x = np.random.random()
+        rand_y = np.sin(rand_x*np.pi)
+        return torch.tensor([rand_x, rand_y]), 0
